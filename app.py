6354814957debe7ded3b6b6d332cd68f9c5f2bb2
Adam Petro <adik6555@gmail.com>
2021-10-02 20:46:12 +0200

    minor changes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+## To-do: Optimization: If existingdata and relayoutdata zoom has not changed, do not rerender everything,
+## just return existing data
+
 
 import dash
 from dash import dcc, html
+from matplotlib.pyplot import scatter
 import plotly.express as px
 import pandas as pd
 from plotly import graph_objects as go
@@ -16,8 +20,7 @@ from sklearn.cluster import DBSCAN
 app = dash.Dash(__name__)
 
 filepath = './dataset/globalterrorismdb_0221dist.csv'
-
-px.set_mapbox_access_token(open(".mapbox_token").read())
+mapbox_access_token = (open(".mapbox_token").read())
 
 df1 = pd.read_csv(filepath, delimiter=';', on_bad_lines='skip', low_memory=False)
 df1.dataframeName = 'globalterrorismdb_0718dist.csv'
@@ -33,46 +36,46 @@ def preprocess(dataset):
     dataset['longitude'] = pd.to_numeric(dataset['longitude'])
     return dataset
 
-def binCoordinates(latitude, longitude, step_size=3):
-    to_bin = lambda x: np.ceil(x / step_size) * step_size
-    latbin = latitude.map(to_bin)
-    lonbin = longitude.map(to_bin)
-    return latbin, lonbin
-
 
 
 def renderMap(dataset, marker_visible=False, center=None, zoom=0):
-    # dataset["latbin"],dataset["lonbin"] = binCoordinates(dataset["latitude"],dataset["longitude"], step_size)
-    dataset = dataset.groupby(['longitude','latitude','region_txt','country_txt']).size().to_frame(name="count").reset_index()
-    dataset['size'] = 4
-    # return px.density_mapbox(dataset, lat='latitude', lon='longitude', hover_name="size",
-    #                  z='size', mapbox_style='light', center=center, zoom=zoom, radius=20, opacity=1)
-    layer1 = go.Densitymapbox(lon=dataset['longitude'], lat=dataset['latitude'], z=dataset['count'],
+    density_dataset = dataset.groupby(['longitude','latitude']).size().to_frame(name="count").reset_index()
+    layer1 = go.Densitymapbox(lon=density_dataset['longitude'], lat=density_dataset['latitude'], z=density_dataset['count'],
                                     colorscale="Viridis")
-    layer2 = go.Scattermapbox(lon=dataset['longitude'], lat=dataset['latitude'], marker=dict(size=dataset['size']))
     layers = [layer1]
+
     if marker_visible:
+        scatter_dataset = dataset.groupby(['longitude','latitude','attacktype1_txt','iyear','imonth','iday']).size().to_frame(name="count").reset_index()
+        marker = dict(
+            opacity=0.3,
+            allowoverlap=True,
+            color="red",
+        )
+        customdata = np.stack((scatter_dataset['attacktype1_txt'].array,scatter_dataset['iday'].array,scatter_dataset['imonth'].array,scatter_dataset['iyear'].array), axis=-1)
+        layer2 = go.Scattermapbox(lon=scatter_dataset['longitude'], lat=scatter_dataset['latitude'], marker=marker,  customdata=customdata, hovertemplate = "<b>%{customdata[3]}-%{customdata[2]}-%{customdata[1]}</b><br><br>%{customdata[0]}")
         layers.append(layer2)
-    fig = go.Figure(data=layers)
+
+    fig = go.Figure(data=layers, layout=go.Layout(autosize=True,
+            showlegend=False,
+            mapbox=dict(
+                accesstoken=mapbox_access_token,
+                style="basic",
+            ),))
     if center==None:
         center = {"lat":0, "lon":0}
     if zoom==None:
         zoom = 1
-    fig.update_layout(mapbox_style="stamen-terrain",
-                    mapbox_zoom=zoom, mapbox_center = center)
+    fig.update_layout(mapbox_zoom=zoom, mapbox_center = center)
     return fig
  
-# a = df1.groupby(['latitude','longitude','country_txt']).size().to_frame(name="count").reset_index()
 
 
 
 
-# groups = a.groupby(("latbin", "lonbin",))
 df1 = preprocess(df1)
 fig = renderMap(df1)
 print("reload")
 
-# fig.show()
 
 
 
@@ -84,28 +87,24 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='example-graph',
+        id='main-map',
         figure=fig
     ),
-    html.Div(id='container', children = []),
-    html.Div(id="my-output")
 ])
 
-@app.callback(Output('example-graph', 'figure'),
-              [Input('example-graph', 'relayoutData')],
-              [State("example-graph","figure")])
+@app.callback(Output('main-map', 'figure'),
+              [Input('main-map', 'relayoutData')],
+              [State("main-map","figure")])
 def display_relayout_data(relayoutData, existingState):
     if relayoutData is not None:
         j = relayoutData
         if 'mapbox.zoom' in j:
-            print(relayoutData)
             if j['mapbox.zoom']>2.5:
                 fig = renderMap(df1, marker_visible=True, center = j['mapbox.center'], zoom=j['mapbox.zoom'])
                 return fig
             else:
-                return existingState
+                return renderMap(df1, center = j['mapbox.center'], zoom=j['mapbox.zoom'])
     return existingState
-            # print(relayoutData)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
